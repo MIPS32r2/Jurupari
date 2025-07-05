9f34b04af7b6e3f01cef80f0321b1c3429c785c5
MIPS32r2 <mips32r2@fsociety>
2025-07-05 04:26:21 -0300

    Diretório de build (não versionado)

---
+++ build/CMakeFiles/jurupari.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/mips32r2/Jurupari/src/config.cpp" "CMakeFiles/jurupari.dir/src/config.cpp.o" "gcc" "CMakeFiles/jurupari.dir/src/config.cpp.o.d"
+  "/home/mips32r2/Jurupari/src/frequencia_combinacoes.cpp" "CMakeFiles/jurupari.dir/src/frequencia_combinacoes.cpp.o" "gcc" "CMakeFiles/jurupari.dir/src/frequencia_combinacoes.cpp.o.d"
+  "/home/mips32r2/Jurupari/src/main.cpp" "CMakeFiles/jurupari.dir/src/main.cpp.o" "gcc" "CMakeFiles/jurupari.dir/src/main.cpp.o.d"
+  "/home/mips32r2/Jurupari/src/mutator.cpp" "CMakeFiles/jurupari.dir/src/mutator.cpp.o" "gcc" "CMakeFiles/jurupari.dir/src/mutator.cpp.o.d"
+  "/home/mips32r2/Jurupari/src/paralelo.cpp" "CMakeFiles/jurupari.dir/src/paralelo.cpp.o" "gcc" "CMakeFiles/jurupari.dir/src/paralelo.cpp.o.d"
+  "/home/mips32r2/Jurupari/src/transformations.cpp" "CMakeFiles/jurupari.dir/src/transformations.cpp.o" "gcc" "CMakeFiles/jurupari.dir/src/transformations.cpp.o.d"
+  "/home/mips32r2/Jurupari/src/utils.cpp" "CMakeFiles/jurupari.dir/src/utils.cpp.o" "gcc" "CMakeFiles/jurupari.dir/src/utils.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
